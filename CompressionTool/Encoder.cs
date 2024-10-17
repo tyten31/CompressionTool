5cb0de0716e8170651f722b4e58363f43bf57c34
ttenney <ttenney@arrowinternational.com>
2024-10-17 15:15:29 -0700

    Compression Tool

---
+++ CompressionTool/Encoder.cs
@@ -0,0 +1,47 @@
+﻿namespace CompressionTool
+{
+    internal class Encoder
+    {
+        public void Encode(string path)
+        {
+            var charFrequency = GetCharacterFrequency(path);
+
+            foreach (var frequency in charFrequency)
+            {
+                Console.WriteLine($"{frequency.Key}: {frequency.Value}");
+            }
+        }
+
+        private List<KeyValuePair<char, int>> GetCharacterFrequency(string path)
+        {
+            var dictionary = new Dictionary<char, int>();
+
+            if (File.Exists(path))
+            {
+                using StreamReader read = new(path);
+                string? line;
+
+                while ((line = read.ReadLine()) != null)
+                {
+                    foreach (var key in line)
+                    {
+                        if (dictionary.TryGetValue(key, out int value))
+                        {
+                            dictionary[key] = value + 1;
+                        }
+                        else
+                        {
+                            dictionary.Add(key, 1);
+                        }
+                    }
+                }
+            }
+
+            var list = dictionary.ToList();
+
+            list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+            return list;
+        }
+    }
+}
---
+++ CompressionTool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace CompressionTool
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var encoder = new Encoder();
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "text.txt");
+
+            encoder.Encode(path);
+
+
+            Console.ReadLine();
+        }
+    }
+}
